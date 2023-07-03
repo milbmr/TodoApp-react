f68170a38eff4750b4fd8d315454c69057769e13
miloud <miloamg@gmail.com>
2023-07-03 23:53:34 +0100

    fixed animation

--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -5,8 +5,16 @@ export default function TodoItem() {
         <div className="flex justify-between items-center bg-white px-6 py-5 mt-4 rounded text-2xl">
             <p>Im going to clean my room</p>
             <div className="flex justify-between items-center gap-6">
-                <BsFillBookmarkCheckFill className="hover:animate-shake-icon cursor-pointer" color="green" size="1.8rem" />
-                <BsTrash color="red" size="1.8rem" />
+                <BsFillBookmarkCheckFill
+                    className="hover:animate-shake ease-in-out cursor-pointer"
+                    color="green"
+                    size="1.8rem"
+                />
+                <BsTrash
+                    className="hover:animate-shake ease-in-out cursor-pointer"
+                    color="red"
+                    size="1.8rem"
+                />
             </div>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,22 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
+            animation: {
+                shake: "shake 1s ease-in-out infinite",
+            },
             keyframes: {
                 shake: {
-                    "0%": {
-                        transform: "translateY(0)",
+                    "0%, 100%": {
+                        transform: "translateY(-10%)",
+                        "animation-timing-function":
+                            "cubic-bezier(0.8, 0, 1, 1)",
                     },
                     "50%": {
-                        transform: "translateY(-10%)",
-                    },
-                    "100%": {
                         transform: "translateY(0)",
+                        "animation-timing-function":
+                            "cubic-bezier(0, 0, 0.2, 1)",
                     },
                 },
-            },
-            aniamtion: {
-                "shake-icon": "shake 1s ease-in-out infinite",
             },
         },
     },
