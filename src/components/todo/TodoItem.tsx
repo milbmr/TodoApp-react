1a6edd66158983d74a066d0258d8fe5258e7c251
miloud <miloamg@gmail.com>
2023-07-04 22:33:06 +0100

    added form

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -2,7 +2,7 @@ import { BsTrash, BsFillBookmarkCheckFill } from "react-icons/bs";
 
 export default function TodoItem({ todo }: { todo: string }) {
     return (
-        <div className="flex justify-between items-center bg-white px-6 py-5 mt-4 rounded text-2xl">
+        <div className="animate-flip backface-visible flex justify-between items-center bg-white px-6 py-5 mt-4 rounded text-2xl">
             <p>{todo}</p>
             <div className="flex justify-between items-center gap-6">
                 <BsFillBookmarkCheckFill
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+/* eslint-disable */
+const plugin = require("tailwindcss/plugin");
 /** @type {import('tailwindcss').Config} */
+
+const backfaceVisibility = plugin(function ({ addUtilities }) {
+    addUtilities({
+        ".backface-visible": {
+            "backface-visibility": "visible",
+        },
+        ".backface-hidden": {
+            "backface-visibility": "hidden",
+        },
+    });
+});
+
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
             animation: {
                 shake: "shake 1s ease-in-out infinite",
+                flip: "flip .5s ease-in-out",
             },
             keyframes: {
                 shake: {
@@ -19,8 +34,37 @@ export default {
                             "cubic-bezier(0, 0, 0.2, 1)",
                     },
                 },
+                flip: {
+                    from: {
+                        transform:
+                            "perspective(400px) rotate3d(1, 0, 0, 90deg)",
+                        "animation-timing-function": "ease-in",
+                        opacity: "0",
+                    },
+
+                    "40%": {
+                        transform:
+                            "perspective(400px) rotate3d(1, 0, 0, -20deg)",
+                        "animation-timing-function": "ease-in",
+                    },
+
+                    "60%": {
+                        transform:
+                            "perspective(400px) rotate3d(1, 0, 0, 10deg)",
+                        opacity: "1",
+                    },
+
+                    "80%": {
+                        transform:
+                            "perspective(400px) rotate3d(1, 0, 0, -5deg)",
+                    },
+
+                    to: {
+                        transform: "perspective(400px)",
+                    },
+                },
             },
         },
     },
-    plugins: [],
+    plugins: [backfaceVisibility],
 };
